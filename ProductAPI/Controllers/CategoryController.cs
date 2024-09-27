81a65308e6e1de23c7a4acda5ea27f728891488b
muhammed.durus <muhammed.durus@MDURUS-CNB>
2024-09-27 14:24:35 +0300

    demo project

---
+++ ProductAPI/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Services.Interface;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public ActionResult GetList()
+        {
+            return Ok(_categoryService.GetList());
+        }
+        
+        [HttpPost]
+        public ActionResult AddCategory(Category entity)
+        {
+            _categoryService.AddCategory(entity);
+            return Ok();
+        }
+    }
+}
